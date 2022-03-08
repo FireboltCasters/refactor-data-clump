7dadae005240699a9f762abeaabf772b0100019e
Nils <nilsbaumgartner1994@gmail.com>
2022-03-08 19:51:09 +0100

    Update

--- src/main/kotlin/com/github/nilsbaumgartner1994/refactordataclump/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/nilsbaumgartner1994/refactordataclump/listeners/MyProjectManagerListener.kt
@@ -1,14 +1,29 @@
 package com.github.nilsbaumgartner1994.refactordataclump.listeners
 
-import com.intellij.openapi.components.service
+import com.github.nilsbaumgartner1994.refactordataclump.util.CacheManager
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import com.github.nilsbaumgartner1994.refactordataclump.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
     override fun projectOpened(project: Project) {
-        println("Project Opened");
-        project.service<MyProjectService>()
+        // Ensure this isn't part of testing
+        if (ApplicationManager.getApplication().isUnitTestMode) {
+            return
+        }
+        println("MyProjectManagerListener - reset the cache to recreate it for the new project");
+        // reset the cache to recreate it for the new project
+        CacheManager.resetIsCacheReady()
+
+        println("MyProjectManagerListener - start creating cache after the project is completely loaded and indexed");
+        //project.service<MyProjectService>()
+
+        // start creating cache after the project is completely loaded and indexed
+        DumbService.getInstance(project).smartInvokeLater(
+            { CacheManager.createClassesListCache(project) }, ModalityState.any()
+        )
     }
 }
